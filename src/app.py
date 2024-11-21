41e2e5be7319d7120512ab7b9c07e461b14d7f25
Alex <da.ek.ned>
2024-11-21 18:42:37 +0300

    сборка проекта в app.py

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from src.views.console_view import ConsoleView
+from src.models.console_model import ConsoleModel
+from src.controllers.console_controller import ConsoleController
+
+
+def main():
+    view = ConsoleView()
+    model = ConsoleModel()
+    controller = ConsoleController(view, model)
+    controller.show_main_menu()
+
+
+if __name__ == '__main__':
+    main()
